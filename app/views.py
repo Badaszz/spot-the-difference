3c882b7a6814699cd8d22a831e5e835c7cbc35f2
Badaszz <134653873+Badaszz@users.noreply.github.com>
2024-07-21 17:52:38 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -23,12 +23,11 @@ def index():
     # Execute if request is POST
     if request.method == "POST":
         # Get uploaded images
-        filename = file_upload.filename
         file_upload = request.files['file_upload']
-        
-        filename2 = file_upload2.filename
+        filename = file_upload.filename
+
         file_upload2 = request.files['file_upload2']
-        
+        filename2 = file_upload2.filename
 
         # Resize and save the uploaded image
         uploaded_image = Image.open(file_upload).resize((250, 160))
@@ -45,10 +44,6 @@ def index():
         # Convert images to grayscale
         original_gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
         uploaded_gray = cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2GRAY)
-        
-        #making another copy do this later
-        #original_image = cv2.imread(os.path.join(app.config['EXISTNG_FILE'], 'original_image.jpg'))
-        #uploaded_image = cv2.imread(os.path.join(app.config['INITIAL_FILE_UPLOADS'], 'uploaded_image.jpg'))
 
         # Calculate structural similarity
         (score, diff) = structural_similarity(original_gray, uploaded_gray, full=True)
@@ -79,32 +74,15 @@ def index():
         x = np.zeros((img_height, 10, 3), np.uint8)
         result1 = np.hstack((original_image, x, uploaded_image))
 
-        # Second set of contours
-        #cnts2 = cv2.findContours(dilate_inv.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        #cnts2 = imutils.grab_contours(cnts2)
-
-        #for contour in cnts2:
-            #if cv2.contourArea(contour) > 10:
-                #x, y, w, h = cv2.boundingRect(contour)
-                #cv2.rectangle(original_image, (x, y), (x + w, y + h), (255, 0, 0), 1)
-                #cv2.rectangle(uploaded_image, (x, y), (x + w, y + h), (255, 0, 0), 1)
-
-        # Display images with rectangles highlighting differences
-        #result2 = np.hstack((original_image, x, uploaded_image))
-
         # Save generated images (if needed)
         cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_differences_1.jpg'), result1)
-        #cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_differences_2.jpg'), result1)
-
-        # Calculate number of differences
-        #p = len(cnts2)
-        #c = 1 if p > 1 else 0  # Condition for plural
 
         # Return result to template
-        return render_template('index.html')
+        return render_template('index.html', pred=str(round(score * 100, 2)) + '% similarity')
 
     return render_template('index.html')  # Default return for GET request
 
 # Main function
 if __name__ == '__main__':
     app.run(debug=True)
+
