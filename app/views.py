7635e2eeeb1253b157ac0881b828889ee41a2ab8
Badaszz <134653873+Badaszz@users.noreply.github.com>
2024-07-21 17:56:59 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -23,11 +23,14 @@ def index():
     # Execute if request is POST
     if request.method == "POST":
         # Get uploaded images
-        file_upload = request.files['file_upload']
         filename = file_upload.filename
+        file_upload = request.files['file_upload']
+        
+
+        filename2 = file_upload2.filename
 
         file_upload2 = request.files['file_upload2']
-        filename2 = file_upload2.filename
+        
 
         # Resize and save the uploaded image
         uploaded_image = Image.open(file_upload).resize((250, 160))
