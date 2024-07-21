cef465ac7b83d2a5e59e7838734af2a3b6dca47a
Badaszz <134653873+Badaszz@users.noreply.github.com>
2024-07-21 18:19:09 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -23,21 +23,22 @@ def index():
     # Execute if request is POST
     if request.method == "POST":
         # Get uploaded images
-        filename = file_upload.filename
-        file_upload = request.files['file_upload']
         
-
+        file_upload = request.files['file_upload']
+        filename = file_upload.filename
+        
+        file_upload2 = request.files['file_upload2']
         filename2 = file_upload2.filename
 
-        file_upload2 = request.files['file_upload2']
+        
         
 
         # Resize and save the uploaded image
-        uploaded_image = Image.open(file_upload).resize((250, 160))
+        uploaded_image = Image.open(filename).resize((250, 160))
         uploaded_image.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], 'uploaded_image.jpg'))
 
         # Resize and save the original image to ensure both uploaded and original match in size
-        original_image = Image.open(file_upload2).resize((250, 160))
+        original_image = Image.open(filename2).resize((250, 160))
         original_image.save(os.path.join(app.config['EXISTNG_FILE'], 'original_image.jpg'))
 
         # Read uploaded and original images as arrays
